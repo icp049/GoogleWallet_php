309cf38ba89c061607bcf917d953d78bd0e6f12b
IJP <108185265+icp049@users.noreply.github.com>
2025-01-02 10:50:31 -0600

    Update wallet.php

--- wallet.php
+++ wallet.php
@@ -43,6 +43,129 @@ class WalletPass {
         return 'A' . $accountNumber . 'A';
     }
 
+
+
+    //Only use this createPassClass to create the class in Google Wallet API, after class is created delete it in your code
+    //If this part is not deleted you will have a pass class already created error
+
+     public function createPassClass() {
+        $genericClass = [
+            'id' => $this->classId,
+            'classTemplateInfo' => [
+                'cardTemplateOverride' => [
+                    'cardRowTemplateInfos' => [
+                        [
+                            'twoItems' => [
+                                'startItem' => [
+                                    'firstValue' => [
+                                        'fields' => [
+                                            ['fieldPath' => 'object.textModulesData["points"]']
+                                        ]
+                                    ]
+                                ],
+                                'endItem' => [
+                                    'firstValue' => [
+                                        'fields' => [
+                                            ['fieldPath' => 'object.textModulesData["contacts"]']
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'detailsTemplateOverride' => [
+                    'detailsItemInfos' => [
+                        [
+                            'item' => [
+                                'firstValue' => [
+                                    'fields' => [
+                                        ['fieldPath' => 'class.imageModulesData["event_banner"]']
+                                    ]
+                                ]
+                            ]
+                        ],
+                        [
+                            'item' => [
+                                'firstValue' => [
+                                    'fields' => [
+                                        ['fieldPath' => 'class.textModulesData["game_overview"]']
+                                    ]
+                                ]
+                            ]
+                        ],
+                        [
+                            'item' => [
+                                'firstValue' => [
+                                    'fields' => [
+                                        ['fieldPath' => 'class.linksModuleData.uris["official_site"]']
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'imageModulesData' => [
+                [
+                    'mainImage' => [
+                        'sourceUri' => [
+                            'uri' => 'https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/google-io-2021-card.png'
+                        ],
+                        'contentDescription' => [
+                            'defaultValue' => [
+                                'language' => 'en-US',
+                                'value' => 'Google I/O 2022 Banner'
+                            ]
+                        ]
+                    ],
+                    'id' => 'event_banner'
+                ]
+            ],
+            'textModulesData' => [
+                [
+                    'header' => 'Gather points meeting new people at Google I/O',
+                    'body' => 'Join the game and accumulate points in this badge by meeting other attendees in the event.',
+                    'id' => 'game_overview'
+                ]
+            ],
+            'linksModuleData' => [
+                'uris' => [
+                    [
+                        'uri' => 'https://io.google/2022/',
+                        'description' => 'Official I/O \'22 Site',
+                        'id' => 'official_site'
+                    ]
+                ]
+            ]
+        ];
+
+        $authToken = $this->getAuthToken();
+
+        try {
+            $response = $this->client->get("{$this->baseUrl}/genericClass/{$this->classId}", [
+                'headers' => ['Authorization' => "Bearer {$authToken}"]
+            ]);
+            if ($response->getStatusCode() == 200) {
+                echo "Class already exists<br>";
+                echo $response->getBody();
+            }
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            if ($e->getResponse()->getStatusCode() == 404) {
+                // Class does not exist, create it
+                $response = $this->client->post("{$this->baseUrl}/genericClass", [
+                    'headers' => ['Authorization' => "Bearer {$authToken}"],
+                    'json' => $genericClass
+                ]);
+
+                echo "Class insert response<br>";
+                echo $response->getBody();
+            } else {
+                echo $e->getMessage();
+            }
+        }
+    }
+
     public function createPassObject() {
         $randomNumber = rand(1000, 9999);
         $objectId = "{$this->issuerId}.{$randomNumber}";
@@ -116,8 +239,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    $walletPass = new WalletPass('config/walletconfig.json');
-
+    $walletPass = new WalletPass('config/walletconfig.json'); #this is the json file you created from service accounts 
+    echo $walletPass->CreatePassClass();
     echo $walletPass->createPassObject();
 }
 
